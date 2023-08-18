0951a14bd3cbdf8dd27b8e63e63b7a7af3395d06
@Marian386 <mperezam@est.utn.ac.cr>
2023-08-17 21:42:55 -0600

    Test8

--- omniNotes/src/main/java/it/feio/android/omninotes/MainActivity.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/MainActivity.java
@@ -80,6 +80,11 @@ import java.util.HashMap;
 import lombok.Getter;
 import lombok.Setter;
 
+//Nuevo
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 
 public class MainActivity extends BaseActivity implements
     SharedPreferences.OnSharedPreferenceChangeListener {
@@ -109,7 +114,11 @@ public class MainActivity extends BaseActivity implements
     Prefs.getPreferences().registerOnSharedPreferenceChangeListener(this);
 
     initUI();
-  }
+
+    //Nuevo
+    AppCenter.start(getApplication(), "c1145493-5cf7-47b1-a334-e73d89d43996", Analytics.class, Crashes.class);
+
+  } 
 
   @Override
   protected void onPostCreate(@Nullable Bundle savedInstanceState) {
